9033bdbae6f10214177db658cdc1f9d1237e3f4b
Rahul Kumar Yadav <RahulY@quinnox.com>
2019-02-11 11:17:25 +0530

    sd

--- src/app/APIs/reset-pass-apis.service.ts
+++ src/app/APIs/reset-pass-apis.service.ts
@@ -1,40 +1,40 @@
 import { Injectable } from '@angular/core';
 import {Http,Headers} from '@angular/http';
-
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+const   headers= new Headers({'Content-Type': 'application/json', Authorization: 'Basic cmFodWx5OkJlaGFwcHlAMDQyNQ== '})
+;
 @Injectable({
   providedIn: 'root'
 })
+
 export class ResetPassAPIsService {
 
   constructor(private http:Http) { }
-
+  
   getAllConn(){
-    return this.http.get('http://localhost:3000/host/getAllHostDetails')
+    return this.http.get('http://localhost:8081/ResetPassword/getAllConnectionDetails',{headers:headers})
     .map(res=>res.json());
   }
   createConn(hostDetail){
-    var headers=new Headers();
-    headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/host/save',hostDetail,{headers:headers})
-    .map(res=>res.json());
+    return this.http.post('http://localhost:8081/ResetPassword/saveConnectionDetails',hostDetail,{headers:headers})
+    .map(res=>res.text);
   }
 
   updateConn(updateDetail){
-    var headers=new Headers();
-    headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/host/save',updateDetail,{headers:headers})
-    .map(res=>res.json());
+   
+    return this.http.put('http://localhost:8081/ResetPassword/updateConnectionDetailsById/'+updateDetail.id,updateDetail,{headers:headers})
+    .map(res=>res.text);
   }
+  
   getConnById(id){
-    var headers=new Headers();
-    headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/host/save',{id},{headers:headers})
+    return this.http.get('http://localhost:8081/ResetPassword/getConnectionDetailById/'+id.id,{headers:headers})
     .map(res=>res.json());
   }
 
   deleteConnById(id){    
-    return this.http.delete('http://localhost:3000/host/getAllHostDetails')
-    .map(res=>res.json());
+    let body=JSON.stringify({"id":id});
+    return this.http.delete('http://localhost:8081/ResetPassword/deleteConnectionDetailsById/'+id,{headers:headers})
+    .map(res=>res.text());
   }
 }
--- src/app/db-connection/db-connection.component.ts
+++ src/app/db-connection/db-connection.component.ts
@@ -19,67 +19,12 @@ export class DbConnectionComponent implements OnInit {
 
   ngOnInit() {
     this.btnName="Add";
-    // this.resetPassApi.getAllConn()
-    // .subscribe(res=>{   
-    this.connectionList=[
-      {
-          "id": 35,
-          "connectionName": "Conn1",
-          "userName": "User1",
-          "currentPassword": "Pass1",
-          "hostName": "Host1",
-          "port": 1,
-          "serviceName": "Service1",
-          "newPassword": null,
-          "confirmPassword": null
-      },
-      {
-        "id": 37,
-        "connectionName": "Conn2",
-        "userName": "User2",
-        "currentPassword": "Pass2",
-        "hostName": "Host2",
-        "port": 2,
-        "serviceName": "Service2",
-        "newPassword": null,
-        "confirmPassword": null
-    },
-    {
-      "id": 39,
-      "connectionName": "Conn3",
-      "userName": "User3",
-      "currentPassword": "Pass3",
-      "hostName": "Host3",
-      "port": 3,
-      "serviceName": "Service3",
-      "newPassword": null,
-      "confirmPassword": null
-  },
-  {
-    "id": 51,
-    "connectionName": "Conn4",
-    "userName": "User4",
-    "currentPassword": "Pass4",
-    "hostName": "Host4",
-    "port": 4,
-    "serviceName": "Service4",
-    "newPassword": null,
-    "confirmPassword": null
-},
-{
-  "id": 25,
-  "connectionName": "Conn5",
-  "userName": "User5",
-  "currentPassword": "Pass5",
-  "hostName": "Host5",
-  "port": 5,
-  "serviceName": "Service5",
-  "newPassword": null,
-  "confirmPassword": null
-}
+    this.resetPassApi.getAllConn()
+     .subscribe(res=>{ 
+      console.log(res);
+      this.connectionList=res;
 
-  ]   
-    $('#connDetailsTable').DataTable().destroy();
+      $('#connDetailsTable').DataTable().destroy();
       setTimeout(function(){
         $('#connDetailsTable').DataTable({
           responsive: true,
@@ -96,37 +41,42 @@ export class DbConnectionComponent implements OnInit {
         $('.dt-buttons a').removeClass('dt-button').addClass('btn btn-success btn-sm'); 
         $('.dt-buttons').css('padding-left','20px')  ;
         }, 50);  
+     });  
+    
       // });
   }  
   addConnection(){
     this.btnName="Add";
+
   }
 
   editConn(id){
     this.btnName="Update";
     this.resetPassApi.getConnById(id)
     .subscribe(res=>{
-      this.edit=res[0];
+      this.edit=res;
     })
   }
 
   updateConn(type){
     console.log(this.edit);
     if(type=="Add"){
-      this.resetPassApi.createConn(this.edit)
+    this.resetPassApi.createConn(this.edit)
       .subscribe(res=>{
         this.ngOnInit();
-        bootbox.alert('<label>Created Succesfully</label>');   
       })
+     
     }
     else{
       this.resetPassApi.updateConn(this.edit)
       .subscribe(res=>{
         this.ngOnInit();
       })
+      
     }
     $('#modalClose').click();
     this.edit={};
+    
   }
 
   deleteConn(id:any){
@@ -141,20 +91,21 @@ export class DbConnectionComponent implements OnInit {
   deleteFunction(hosts,id){
     this.resetPassApi.deleteConnById(id)
           .subscribe(res=>{
-      if(res.n==1){
-        for(var i=0;i<hosts.length;i++){
-          if(hosts[i]._id==id){            
-            hosts.splice(i,1); 
-            $('#hostDetailsTable').DataTable().destroy();
-            setTimeout(function(){
-              $('#hostDetailsTable').DataTable({
-                responsive: true
-              });              
-              }, 50);               
-            bootbox.alert("<label>Deleted Successfully</label>");
-          }
-        }
-      }
+      // if(res.n==1){
+      //   for(var i=0;i<hosts.length;i++){
+      //     if(hosts[i]._id==id){            
+      //       hosts.splice(i,1); 
+      //       $('#hostDetailsTable').DataTable().destroy();
+      //       setTimeout(function(){
+      //         $('#hostDetailsTable').DataTable({
+      //           responsive: true
+      //         });              
+      //         }, 50);               
+      //       bootbox.alert("<label>Deleted Successfully</label>");
+      //     }
+      //   }
+      // }
+      this.ngOnInit();
     })
   }
 }
